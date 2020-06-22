1a3e0366db6d93533bdf5c51e17d86856fbff292
Mandy <drgnfli6420@yahoo.com>
2020-06-22 14:16:15 -0400

    playlist details working

--- src/modules/PlaylistManager.js
+++ src/modules/PlaylistManager.js
@@ -2,9 +2,10 @@ const remoteURL = "http://localhost:5002"
 
 export default {
   get(id) {
-    return fetch(`${remoteURL}/playlists/${id}`).then(result => result.json())
+    return fetch(`${remoteURL}/playlists/${id}?_embed=songs`).then(result => result.json())
   },
   getAll() {
     return fetch(`${remoteURL}/playlists`).then(result => result.json())
   }
 }
+
